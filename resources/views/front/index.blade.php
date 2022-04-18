8323e345a276cee6b07dbaf520cf613dbe08eacb
mgundogan33 <gundogan.mehmet33@gmail.com>
2022-04-18 11:47:46 +0300

    4

---
+++ resources/views/front/index.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+
+        <div class="row">
+            <div class="col-md-8">
+                Burası soru alanları
+            </div>
+
+            <div class="col-md-4">
+                burası kategorı alanları
+            </div>
+        </div>
+
+
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\front\indexController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::group(['namespace'=>'front'],function(){
+Route::get('/',[indexController::class,'index'])->name('index');
+});
