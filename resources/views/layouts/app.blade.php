15cb9a2a9842608ef69a74aa6966cc0823936c68
mgundogan33 <gundogan.mehmet33@gmail.com>
2022-04-19 21:21:03 +0300

    7

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -88,9 +88,9 @@
             @yield('content')
         </main>
     </div>
-    <script src=""></script>
-
     <script src="https://code.jquery.com/jquery-2.2.4.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
     @yield('footer')
 </body>
 
